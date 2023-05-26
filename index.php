9dc621ac32503b31c525b74ea8b31a1cc1214c30
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-05-26 09:24:31 -0300

    Commit do José

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tabuada</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <form action="tabuada.php" method="get">
+        <h1>Tabuada</h1>
+        <br><label for="number">Escolha um número de 1 a 10</label><br>
+        <input type="number" name="number" min="1" max="10">
+        <input type="submit">
+    </form>
+</body>
+</html>
